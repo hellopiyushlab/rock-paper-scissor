da1baef35233d97f36d27a52909a012e61d96891
Piyush <hellopiyush.lab@gmail.com>
2025-07-07 05:29:40 +0530

    implemented winning losing logic

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function testfunction(){
-    console.log("test successful");
+function computerTurn(playerChoice, max=3) {
+    let computerChoice = Math.floor(Math.random() * max); // 0, 1, or 2
+    console.log(computerChoice);
+    
+    if (playerChoice === computerChoice) {
+        console.log("draw");
+    } else if (computerChoice+1 === playerChoice) {
+        console.log("player wins");
+    } else if (playerChoice+1 === computerChoice) {
+        console.log("computer wins");
+    } else if (playerChoice-2 === computerChoice) {
+        console.log("computer wins");
+    } else if (computerChoice-2 === playerChoice) {
+        console.log("player wins");
+    }
 }
