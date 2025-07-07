64ea5ba2c0406b4d24a143bf1299d9be4625924c
Piyush <hellopiyush.lab@gmail.com>
2025-07-07 05:48:25 +0530

    score count logic applied

--- script.js
+++ script.js
@@ -5,12 +5,34 @@ function computerTurn(playerChoice, max=3) {
     if (playerChoice === computerChoice) {
         console.log("draw");
     } else if (computerChoice+1 === playerChoice) {
-        console.log("player wins");
+        // console.log("player wins");
+        playerWins();
     } else if (playerChoice+1 === computerChoice) {
-        console.log("computer wins");
+        // console.log("computer wins");
+        computerWins();
     } else if (playerChoice-2 === computerChoice) {
-        console.log("computer wins");
+        // console.log("computer wins");
+        computerWins();
     } else if (computerChoice-2 === playerChoice) {
-        console.log("player wins");
+        // console.log("player wins");
+        playerWins();
     }
+}
+
+// const playerScore = document.getElementById("playerScore");
+// const computerScore = document.getElementById("computerScore");
+
+let playerScoreCount = 0;
+let computerScoreCount = 0;
+
+function playerWins() {
+    playerScoreCount++;
+    console.log("Player Score: " + playerScoreCount)
+    document.getElementById("playerScore").innerHTML = "Player's Score: " + playerScoreCount;
+}
+
+function computerWins() {
+    computerScoreCount++;
+    console.log("Computer Score: " + computerScoreCount);
+    document.getElementById("computerScore").innerHTML = "Computer's Score: " + computerScoreCount;
 }
