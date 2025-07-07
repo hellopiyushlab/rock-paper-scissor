a84a1a34f2384872895714b947fd3bd885d173b2
Piyush <hellopiyush.lab@gmail.com>
2025-07-07 07:00:21 +0530

    ADDED BASIC CSS
    Added basic CSS with DOM manipulation.
    Experimenting with temporary borders.
    Score is only being counted and shown right now.

--- script.js
+++ script.js
@@ -2,37 +2,39 @@ function computerTurn(playerChoice, max=3) {
     let computerChoice = Math.floor(Math.random() * max); // 0, 1, or 2
     console.log(computerChoice);
     
+    // 0 -> Rock
+    // 1 -> Paper
+    // 2 -> Scissor
+
+
     if (playerChoice === computerChoice) {
         console.log("draw");
-    } else if (computerChoice+1 === playerChoice) {
-        // console.log("player wins");
+    } 
+    else if (computerChoice+1 === playerChoice) {
         playerWins();
-    } else if (playerChoice+1 === computerChoice) {
-        // console.log("computer wins");
+    } 
+    else if (playerChoice+1 === computerChoice) {
         computerWins();
-    } else if (playerChoice-2 === computerChoice) {
-        // console.log("computer wins");
+    } 
+    else if (playerChoice-2 === computerChoice) {
         computerWins();
-    } else if (computerChoice-2 === playerChoice) {
-        // console.log("player wins");
+    } 
+    else if (computerChoice-2 === playerChoice) {
         playerWins();
     }
 }
-
-// const playerScore = document.getElementById("playerScore");
-// const computerScore = document.getElementById("computerScore");
 
 let playerScoreCount = 0;
 let computerScoreCount = 0;
 
 function playerWins() {
-    playerScoreCount++;
+    playerScoreCount++; // increment the score
     console.log("Player Score: " + playerScoreCount)
-    document.getElementById("playerScore").innerHTML = "Player's Score: " + playerScoreCount;
+    document.getElementById("playerScore").innerHTML = "Player: " + playerScoreCount;
 }
 
 function computerWins() {
-    computerScoreCount++;
+    computerScoreCount++; // increment the score
     console.log("Computer Score: " + computerScoreCount);
-    document.getElementById("computerScore").innerHTML = "Computer's Score: " + computerScoreCount;
+    document.getElementById("computerScore").innerHTML = "Computer: " + computerScoreCount;
 }
